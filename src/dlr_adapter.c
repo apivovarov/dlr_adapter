fe0670c82fb71dc8bdd4b0282a852ee1b3ec415c
Alexander Pivovarov <pivovaa@amazon.com>
2021-11-11 23:31:07 -0800

    Fix duration calculation

--- src/dlr_adapter.c
+++ src/dlr_adapter.c
@@ -1,111 +1,113 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <time.h>
 #include "dlr.h"
 
-#define TICK(X) clock_t X = clock()
-#define TOCK(X, MM) printf("time %s: %g ms.\n", (#MM), (double)(clock() - (X)) / (CLOCKS_PER_SEC / 1000.0))
+#define TICK(X) struct timespec X; clock_gettime(CLOCK_REALTIME, &X)
+#define TOCK(X, X1, MM) struct timespec X1; clock_gettime(CLOCK_REALTIME, &X1);\
+  printf("time %s: %g sec.\n", (#MM), (X1.tv_sec - X.tv_sec) + (X1.tv_nsec - X.tv_nsec) / 1e9)
 
 int CreateDLRModelTT(DLRModelHandle* handle, const char* model_path, int dev_type, int dev_id) {
   TICK(TT);
   int res = CreateDLRModel(handle, model_path, dev_type, dev_id);
-  TOCK(TT, CreateDLRModel);
+  TOCK(TT, TT1, CreateDLRModel);
   return res;
 }
 
 int DeleteDLRModelTT(DLRModelHandle* handle) {
   TICK(TT);
   int res = DeleteDLRModel(handle);
-  TOCK(TT, DeleteDLRModel);
+  TOCK(TT, TT1, DeleteDLRModel);
   return res;
 }
 
 int RunDLRModelTT(DLRModelHandle* handle) {
   TICK(TT);
   int res = RunDLRModel(handle);
-  TOCK(TT, RunDLRModel);
+  TOCK(TT, TT1, RunDLRModel);
   return res;
 }
 
 int GetDLRNumInputsTT(DLRModelHandle* handle, int* num_inputs) {
   TICK(TT);
   int res = GetDLRNumInputs(handle, num_inputs);
-  TOCK(TT, GetDLRNumInputs);
+  TOCK(TT, TT1, GetDLRNumInputs);
   return res;
 }
 
 int SetDLRInputTT(DLRModelHandle* handle, const char* name, const int64_t* shape, const void* input, int dim){
   TICK(TT);
   int res = SetDLRInput(handle, name, shape, input, dim);
-  TOCK(TT, SetDLRInput);
+  TOCK(TT, TT1, SetDLRInput);
   return res;
 }
 
 int GetDLRInputSizeDimTT(DLRModelHandle* handle, int index, int64_t* size, int* dim) {
   TICK(TT);
   int res = GetDLRInputSizeDim(handle, index, size, dim);
-  TOCK(TT, GetDLRInputSizeDim);
+  TOCK(TT, TT1, GetDLRInputSizeDim);
   return res;
 }
 
 int GetDLROutputShapeTT(DLRModelHandle* handle, int index, int64_t* shape) {
   TICK(TT);
   int res = GetDLROutputShape(handle, index, shape);
-  TOCK(TT, GetDLROutputShape);
+  TOCK(TT, TT1, GetDLROutputShape);
   return res;
 }
 
 int GetDLROutputTT(DLRModelHandle* handle, int index, void* out) {
   TICK(TT);
   int res = GetDLROutput(handle, index, out);
-  TOCK(TT, GetDLROutput);
+  TOCK(TT, TT1, GetDLROutput);
   return res;
 }
 
 int GetDLROutputPtrTT(DLRModelHandle* handle, int index, const void** out) {
   TICK(TT);
   int res = GetDLROutputPtr(handle, index, out);
-  TOCK(TT, GetDLROutputPtr);
+  TOCK(TT, TT1, GetDLROutputPtr);
   return res;
 }
 
 int GetDLRNumOutputsTT(DLRModelHandle* handle, int* num_outputs) {
   TICK(TT);
   int res = GetDLRNumOutputs(handle, num_outputs);
-  TOCK(TT, GetDLRNumOutputs);
+  TOCK(TT, TT1, GetDLRNumOutputs);
   return res;
 }
 
 int GetDLROutputSizeDimTT(DLRModelHandle* handle, int index, int64_t* size, int* dim) {
   TICK(TT);
   int res = GetDLROutputSizeDim(handle, index, size, dim);
-  TOCK(TT, GetDLROutputSizeDim);
+  TOCK(TT, TT1, GetDLROutputSizeDim);
   return res;
 }
 
 int GetDLROutputTypeTT(DLRModelHandle* handle, int index, const char** output_type) {
   TICK(TT);
   int res = GetDLROutputType(handle, index, output_type);
-  TOCK(TT, GetDLROutputType);
+  TOCK(TT, TT1, GetDLROutputType);
   return res;
 }
 
 int GetDLROutputNameTT(DLRModelHandle* handle, const int index, const char** name) {
   TICK(TT);
   int res = GetDLROutputName(handle, index, name);
-  TOCK(TT, GetDLROutputName);
+  TOCK(TT, TT1, GetDLROutputName);
   return res;
 }
 
 int GetDLROutputIndexTT(DLRModelHandle* handle, const char* name, int* index) {
   TICK(TT);
   int res = GetDLROutputIndex(handle, name, index);
-  TOCK(TT, GetDLROutputIndex);
+  TOCK(TT, TT1, GetDLROutputIndex);
   return res;
 }
 
 int GetDLROutputByNameTT(DLRModelHandle* handle, const char* name, void* out) {
   TICK(TT);
   int res = GetDLROutputByName(handle, name, out);
-  TOCK(TT, GetDLROutputByName);
+  TOCK(TT, TT1, GetDLROutputByName);
   return res;
 }
